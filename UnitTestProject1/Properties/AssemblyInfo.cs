3d1aac8581bab87c4152b555d2de353237fccec6
Harjan9211 <41310824+Harjan9211@users.noreply.github.com>
2018-08-07 10:28:03 +1200

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bfd571fb-6685-40de-9e3e-978877952911")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Media;
+using Shooting_Game;
+using Shooting_Game.Properties;
+using Mole_Shooter;
+using Mole_Shooter.Properties;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SoundPlayer soundPlayer = new SoundPlayer();
+            Assert.IsTrue(soundPlayer != null);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SoundPlayer soundPlayer1 = new SoundPlayer();
+            Assert.IsTrue(soundPlayer1 != null);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            SoundPlayer simpleSound = new SoundPlayer();
+            Assert.IsTrue(simpleSound != null);
+        }
+    }
+  
+}
